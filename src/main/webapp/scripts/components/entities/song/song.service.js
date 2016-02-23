e2141fab1d3a255865ee0bb1053acaf53c3eaf3d
Miroslav Istvan <miroslav.istvan@gmail.com>
2016-02-23 07:47:37 +0100

    Usage of WP-API for song entity. Use slug property to check if it is working fine.

--- src/main/webapp/scripts/components/entities/song/song.service.js
+++ src/main/webapp/scripts/components/entities/song/song.service.js
@@ -2,7 +2,7 @@
 
 angular.module('spevnikApp')
     .factory('Song', function ($resource, DateUtils) {
-        return $resource('api/songs/:id', {}, {
+        return $resource('http://mistvan.spolocenstvomajak.sk/spevnik?rest_route=/wp/v2/song/:id', {}, {
             'query': { method: 'GET', isArray: true},
             'get': {
                 method: 'GET',
